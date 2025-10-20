21ca995c4d839255ad80eba7c3eeb3f6c08b96db
ljj0210 <ljj0210@gmail.com>
2025-10-20 22:16:33 +0900

    feat(admin): 프로젝트 목록 그리드/필터/액션 버튼 기본 구현 및 페이지 연결

--- frontend/src/admin/pages/ProjectList.tsx
+++ frontend/src/admin/pages/ProjectList.tsx
@@ -1,24 +1,6 @@
 import React from 'react';
-import { Card, Typography } from 'antd';
+import ProjectList from '../components/projects/ProjectList';
 
-const { Title } = Typography;
-
-/**
- * 프로젝트 목록 컴포넌트
- */
-const ProjectList: React.FC = () => {
-    return (
-        <div>
-            <Title level={2} style={{ marginBottom: '24px' }}>
-                프로젝트 관리
-            </Title>
-            
-            <Card>
-                <p>프로젝트 목록이 여기에 표시됩니다.</p>
-                <p>구현 예정: 프로젝트 CRUD, 필터링, 검색 기능</p>
-            </Card>
-        </div>
-    );
-};
-
-export default ProjectList;
+export default function ProjectListPage() {
+    return <ProjectList />;
+}
