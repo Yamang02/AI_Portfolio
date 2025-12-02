18392b966eb838d9c29fe349e60354e56df0b7f6
ljj0210 <ljj0210@gmail.com>
2025-12-03 00:13:33 +0900

    fix(build): 빌드오류

--- frontend/src/admin/entities/cloud-usage/index.ts
+++ frontend/src/admin/entities/cloud-usage/index.ts
@@ -2,6 +2,9 @@ export * from './model/cloudUsage.types';
 export * from './api/cloudUsageApi';
 export * from './api/useCloudUsageQuery';
 
+// 명시적으로 export (빌드 에러 방지)
+export { useSearchUsageTrend } from './api/useCloudUsageQuery';
+
 
 
 
